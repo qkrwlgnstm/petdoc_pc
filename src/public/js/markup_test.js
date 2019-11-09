a1c22b83314cc50f6eb33adedbcecf638097f864
박지훈 <qkrwlgnstm@naver.com>
2019-11-09 18:37:44 +0900

    PC 업로드

--- src/public/js/markup_test.js
+++ src/public/js/markup_test.js
@@ -12,12 +12,26 @@ $(document).ready(function(){
         $('.autocomplet_list').toggleClass("show");
 		$('.keyword_layer').removeClass("show");
     })
-
-
-
+    $('.category_box_area').click(function(e){
+        $('.category_box_area').toggleClass("show");
+        $('.counseling_category').toggleClass("show");
+        $('.layer_category').toggleClass("show");
+    })
     $('.btn_site').click(function(e){
         $('.family_site_layer').toggleClass("show");
     })
+    $('.common-btn-share').click(function(e){
+        $('.share_layer').toggleClass("show");
+    })
+    $('.like_area').click(function(e){
+        $('.like_area').toggleClass("show");
+    })
+    $('.btn_bookmark_area').click(function(e){
+		$('.btn_bookmark_area').toggleClass("show");
+		$('.bookmark_txt_area').toggleClass("show");
+    })
+
+
 
 
 
@@ -28,14 +42,11 @@ $(document).ready(function(){
     $('.btn_recommend').click(function(e){
 		$('.btn_recommend').toggleClass("show");
     })
-    $('.btn_bookmark').click(function(e){
-		$('.btn_bookmark').toggleClass("show");
-    })
     $('.filter_box_area').click(function(e){
 		$('.filter_box').toggleClass("show");
     })
     $('.btn_login').click(function(e){
-		$('.layer_menu  .dimmed').toggleClass("show");
+		$('.layer_menu .dimmed').toggleClass("show");
     })
     $('.btn_counseling').click(function(e){
 		$('.dimmed').toggleClass("show");
@@ -48,9 +59,6 @@ $(document).ready(function(){
     })
     $('.search_header .search_box .input_text').keydown(function(e){
         $('.delete_button').addClass("show");
-    })
-    $('.like_area').click(function(e){
-        $('.like_area').toggleClass("show");
     })
     $('.reply_input_area').click(function(e){
         $('.dimmed').toggleClass("show");
